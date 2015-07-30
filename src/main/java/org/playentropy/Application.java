5cf0ae0dda1db7974361b2979d68588a9d8040e5
Mogria <m0gr14@gmail.com>
2015-07-30 18:31:01 +0200

    the application now writes it's PID to a file called playentropy.pid

--- src/main/java/org/playentropy/Application.java
+++ src/main/java/org/playentropy/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 
 @Configuration
 @EnableAutoConfiguration
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new ApplicationPidListener("playentropy.pid"));
+        app.run(args);
     }
 }
