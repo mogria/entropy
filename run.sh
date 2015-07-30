e8870e566d8c13baa239d42d6ab8ba177b595107
Mogria <m0gr14@gmail.com>
2015-07-30 19:17:03 +0200

    add script to run application

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+start() {
+    mvn spring-boot:run &
+}
+
+stop() {
+    PIDFILE="playentropy.pid"
+    if [ -f "$PIDFILE" ]; then
+        pid="`cat "$PIDFILE"`"
+        if [ "`uname -o`" = "Cygwin" ]; then
+            # cygwin can't simply kill win32 processes (which java in most
+            # cases is). So we need to avoid the built-in kill and use
+            # the one provided by cygwin so the -f option is available
+            /bin/kill -f "$pid"
+        else
+            kill "$pid"
+        fi
+        rm "$PIDFILE"
+    fi
+}
+
+reload() {
+        mvn compile
+}
+
+stop
+start
+while read line; do
+    if [ "$line" = "r" ]; then
+        stop
+        start
+    elif [ "$line" = "q" ]; then
+        stop
+        exit 0
+    else
+        reload
+    fi
+done
