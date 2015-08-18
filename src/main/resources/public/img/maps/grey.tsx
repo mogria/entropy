170ff198a3c788a78cff521eedf6697ad756158f
Mogria <m0gr14@gmail.com>
2015-08-18 21:11:36 +0200

    add first simple tileset and map

---
+++ src/main/resources/public/img/maps/grey.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grey" tilewidth="64" tileheight="64">
+ <image source="../grey-tiles.png" width="448" height="320"/>
+ <terraintypes>
+  <terrain name="ground" tile="6"/>
+  <terrain name="high" tile="11"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,1"/>
+ <tile id="1" terrain="0,0,1,1"/>
+ <tile id="2" terrain="0,0,1,0"/>
+ <tile id="3" terrain="0,0,0,1"/>
+ <tile id="4" terrain="0,0,1,1"/>
+ <tile id="5" terrain="0,0,1,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,1,0,1"/>
+ <tile id="8" terrain="1,1,1,1"/>
+ <tile id="9" terrain="1,0,1,0"/>
+ <tile id="10" terrain="0,1,0,1"/>
+ <tile id="11" terrain="1,1,1,1"/>
+ <tile id="12" terrain="1,0,1,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,1,0,0"/>
+ <tile id="15" terrain="1,1,0,0"/>
+ <tile id="16" terrain="1,0,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="-0.333333" y="0.333333" width="32.6667" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17" terrain="0,1,0,0"/>
+ <tile id="18" terrain="1,1,0,0"/>
+ <tile id="19" terrain="1,0,0,0"/>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="21" terrain="1,1,1,0"/>
+ <tile id="22" terrain="1,1,0,1"/>
+ <tile id="28" terrain="1,0,1,1"/>
+ <tile id="29" terrain="0,1,1,1"/>
+</tileset>
